9ac4c1ad4ee87ee2ac2588deb1a22646a32ce492
2022wb86803 <2022wb86803@wilp.bits-pilani.ac.in>
2026-09-25 03:45:10 +0530

    Test continuous integration trigger

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("BITS DevOps Lab - Jenkins Maven Build");
         System.out.println("Student ID: 2022wb86803");
+        System.out.println("Continuous Integration Test - Build Triggered Successfully");
     }
 }
