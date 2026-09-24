9e2c4fbad17120fbf90d64e4a263737341d6bb29
2022wb86803 <2022wb86803@wilp.bits-pilani.ac.in>
2026-09-25 03:08:02 +0530

    Initial Maven Project

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,9 @@
+package com.bits;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("BITS DevOps Lab - Jenkins Maven Build");
+        System.out.println("Student ID: 2022wb86803");
+    }
+}
